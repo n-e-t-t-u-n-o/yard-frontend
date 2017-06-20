eb97cdd8c15d12a9adad6d44134026e392e5feee
“Ilya <kotuko@gmail.com>
2017-06-20 12:47:25 +0300

    react helmet installed

--- src/GlobalHeader.js
+++ src/GlobalHeader.js
@@ -1,6 +1,53 @@
 import React from 'react';
+import logo from './img/compass-logo.svg';
+import styled from 'styled-components'
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
+const GlobalHeader = styled.header`
+  display: flex;
+  background-color: #fff;
+`;
+
+const InnerPageHeader = GlobalHeader.extend`
+  border-bottom: 1px solid #eaebf0;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const GlobalHeaderLogo = styled.img`
+  margin: 0;
+`
+
+const HeaderNav = styled.nav`
+  display: flex;
+  margin-top: 1.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+const HeaderNavLink = styled.a`
+  margin-left: 2rem;
+  font-size: 1rem;
+  text-decoration: none;
+  line-height: 1;
+  color: #3e4247;
+`;
 
 export default () => {
   return (
+    <GlobalHeader>
+      <Grid>
+        <Wrapper>
+          <GlobalHeaderLogo src={logo} />
+          <HeaderNav>
+            <HeaderNavLink href="" title="Buy">Buy</HeaderNavLink>
+            <HeaderNavLink href="" title="Rent">Rent</HeaderNavLink>
+            <HeaderNavLink href="" title="Our Agents">Our Agents</HeaderNavLink>
+          </HeaderNav>
+        </Wrapper>
+      </Grid>
+    </GlobalHeader>
   )
 }
