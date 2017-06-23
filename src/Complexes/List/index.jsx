04cbddfc7533193bcd8adcc4f2a65f0ace9c1ccf
“Ilya <kotuko@gmail.com>
2017-06-24 01:47:03 +0300

    fix: removed unnecessary components and styles from Complexes/List/index.jsx for they were defined earlier in Card.jsx

--- src/Complexes/List/index.jsx
+++ src/Complexes/List/index.jsx
@@ -13,12 +13,6 @@ const CardsList = styled.section`
   padding-bottom: 1rem;
 `;
 
-const CardText = styled.p`
-  font-size: 1rem;
-  line-height: 1.5;
-  color: #3e4247;
-`;
-
 export default () =>
   (
     <BodyClassName className="complexes">
@@ -27,32 +21,30 @@ export default () =>
         <Discover/>
         <CardsList>
           <Grid>
+
             <Card id={1} area="South Beach, San Francisco" title="The Harrison">
-              <CardText>
-                The Lewis Steel Building is a masterful industrial conversion
-                located in the heart of Williamsburg. Located at 76 North 4th Street, the
-                former 1930&apos;s steel factory has been transformed into 83
-                individually unique and luxury loft apartments.
-              </CardText>
+              The Lewis Steel Building is a masterful industrial conversion
+              located in the heart of Williamsburg. Located at 76 North 4th Street, the
+              former 1930&apos;s steel factory has been transformed into 83
+              individually unique and luxury loft apartments.
             </Card>
+
             <Card id={2} area="Midtown East, Manhattan" title="100 East 53rd Street">
-              <CardText>
-                One Hundred East Fifty Third Street by Foster + Partners is a
-                limited collection of modern residences in Midtown Manhattan&apos;s
-                Cultural District. The 94 residences ranging from alcove lofts to
-                four bedrooms within the 63-story tower are generously proportioned.
-              </CardText>
+              One Hundred East Fifty Third Street by Foster + Partners is a
+              limited collection of modern residences in Midtown Manhattan&apos;s
+              Cultural District. The 94 residences ranging from alcove lofts to
+              four bedrooms within the 63-story tower are generously proportioned.
             </Card>
+
             <Card id={3} area="Nolita, Manhattan" title="152 Elizabeth">
-              <CardText>
-                152 Elizabeth is an ultra-luxury condominium building—the first in
-                New York City designed by Japanese master architect Tadao Ando.
-                Located at the corner of Kenmare and Elizabeth Streets in Nolita,
-                the 32,000-square-foot building will stand as a profound
-                architectural statement and embrace the industrial character of the
-                neighborhood.
-              </CardText>
+              152 Elizabeth is an ultra-luxury condominium building—the first in
+              New York City designed by Japanese master architect Tadao Ando.
+              Located at the corner of Kenmare and Elizabeth Streets in Nolita,
+              the 32,000-square-foot building will stand as a profound
+              architectural statement and embrace the industrial character of the
+              neighborhood.
             </Card>
+
           </Grid>
         </CardsList>
       </div>
